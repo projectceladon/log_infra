b2568b9895f307ccfea0866f84feeafe4e775c14
Tony Goubert <tonyx.goubert@intel.com>
2012-01-26 13:49:38 +0100

    [PORT FROM R2] amtl modification traces in coredump part
    BZ: 21260
    
    Thanks these modifications, Artemis traces are enabled.
    
    Change-Id: Ic273e3cedc3d4c96f35d68670731f28c94b3b9ef
    Orig-Change-Id: Ie410f4ba4dbb9e5c234f7d1d63265138af49c3a3
    Signed-off-by: Tony Goubert <tonyx.goubert@intel.com>
    Reviewed-on: http://android.intel.com:8080/33316
    Reviewed-by: Predon, Frederic <frederic.predon@intel.com>
    Reviewed-by: Robert, Denis <denis.robert@intel.com>
    Reviewed-by: Lebsir, SamiX <samix.lebsir@intel.com>
    Tested-by: Lebsir, SamiX <samix.lebsir@intel.com>
    Reviewed-by: buildbot <buildbot@intel.com>
    Tested-by: buildbot <buildbot@intel.com>

--- amtl/src/com/intel/amtl/Trace_in_coredumpActivity.java
+++ amtl/src/com/intel/amtl/Trace_in_coredumpActivity.java
@@ -33,16 +33,13 @@ import android.widget.TextView;
 
 public class Trace_in_coredumpActivity extends Activity {
 
-    private Button button_disable_ma_artemis_coredump;
-    private Button button_enable_ma_artemis_coredump;
-    private Button button_apply_ma_artemis_coredump;
-    private Button button_disable_ma_artemis_digrf_coredump;
-    private Button button_enable_ma_artemis_digrf_coredump;
-    private Button button_apply_ma_artemis_digrf_coredump;
-    private Button button_apply_activate_coredump;
+    private Button button_trace_ma_coredump, button_trace_ma_artemis_coredump;
+    private Button button_trace_ma_artemis_digrf_coredump, button_trace_none_coredump;
+    private Button button_apply_trace_coredump;
+
     private ProgressDialog progressDialog;
     private TextView coredump_text;
-    private int trace_value;
+    private int xsio_value;
     Runtime rtm=java.lang.Runtime.getRuntime();
 
     void writeSimple(String iout,String ival) throws IOException {
@@ -56,153 +53,94 @@ public class Trace_in_coredumpActivity extends Activity {
         coredump_text.setText("Your board need a HARDWARE reboot");
     }
 
-    private void trace_unavailable() {
-        coredump_text.setVisibility(View.VISIBLE);
-        coredump_text.setText("You can't enable it, disable others traces BEFORE");
-    }
-
-    private void wrong_trace_enabled() {
-        coredump_text.setVisibility(View.VISIBLE);
-        coredump_text.setText("DISABLE MA & Artemis traces THEN Enable MA & Artemis & Digrf traces");
-    }
-
-    private void trace_not_enabled() {
-        coredump_text.setVisibility(View.VISIBLE);
-        coredump_text.setText("Enable MA & Artemis & Digrf traces BEFORE");
-    }
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.trace_in_coredump);
 
-        button_disable_ma_artemis_coredump = (Button) findViewById(R.id.disable_ma_artemis_coredump_button);
-        button_enable_ma_artemis_coredump = (Button) findViewById(R.id.enable_ma_artemis_coredump_button);
-        button_apply_ma_artemis_coredump = (Button) findViewById(R.id.apply_ma_artemis_coredump_button);
-        button_disable_ma_artemis_digrf_coredump = (Button) findViewById(R.id.disable_ma_artemis_digrf_coredump_button);
-        button_enable_ma_artemis_digrf_coredump = (Button) findViewById(R.id.enable_ma_artemis_digrf_coredump_button);
-        button_apply_ma_artemis_digrf_coredump = (Button) findViewById(R.id.apply_ma_artemis_digrf_coredump_button);
-        button_apply_activate_coredump = (Button) findViewById(R.id.apply_activate_coredump_button);
+        button_trace_ma_coredump = (Button) findViewById(R.id.trace_ma_coredump_button);
+        button_trace_ma_artemis_coredump = (Button) findViewById(R.id.trace_ma_artemis_coredump_button);
+        button_trace_ma_artemis_digrf_coredump = (Button) findViewById(R.id.trace_ma_artemis_digrf_coredump_button);
+        button_trace_none_coredump = (Button) findViewById(R.id.trace_none_coredump_button);
+        button_apply_trace_coredump = (Button) findViewById(R.id.apply_trace_coredump_button);
         coredump_text=(TextView) findViewById(R.id.text_coredump);
 
         /*Get the between instance stored values*/
         SharedPreferences preferences = getPreferences(MODE_PRIVATE);
-        ((CompoundButton) button_disable_ma_artemis_coredump).setChecked(preferences.getBoolean("button_disable_ma_artemis_coredump_value", true));
-        ((CompoundButton) button_enable_ma_artemis_coredump).setChecked(preferences.getBoolean("button_enable_ma_artemis_coredump_value", false));
-        ((CompoundButton) button_disable_ma_artemis_digrf_coredump).setChecked(preferences.getBoolean("button_disable_ma_artemis_digrf_coredump", true));
-        ((CompoundButton) button_enable_ma_artemis_digrf_coredump).setChecked(preferences.getBoolean("button_enable_ma_artemis_digrf_coredump_value", false));
-        trace_value = preferences.getInt("trace_value", trace_value);
+        ((CompoundButton) button_trace_ma_coredump).setChecked(preferences.getBoolean("button_trace_ma_coredump_value", false));
+        ((CompoundButton) button_trace_ma_artemis_coredump).setChecked(preferences.getBoolean("button_trace_ma_artemis_coredump_value", false));
+        ((CompoundButton) button_trace_ma_artemis_digrf_coredump).setChecked(preferences.getBoolean("button_trace_ma_artemis_digrf_coredump_value", false));
+        ((CompoundButton) button_trace_none_coredump).setChecked(preferences.getBoolean("button_trace_none_coredump_value", true));
+        xsio_value = preferences.getInt("xsio_value", 0);
 
-        /*Listener for button_apply_ma_artemis_coredump*/
-        button_apply_ma_artemis_coredump.setOnClickListener(new View.OnClickListener() {
+        /*Listener for button_apply_trace*/
+        button_apply_trace_coredump.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View arg0) {
-                if ((((CompoundButton) button_enable_ma_artemis_digrf_coredump).isChecked())||((((CompoundButton) button_enable_ma_artemis_coredump).isChecked()) && (trace_value!=0))) {
-                    /*Trace already running ?*/
-                    trace_unavailable();
-                } else {
-                    progressDialog = ProgressDialog.show(Trace_in_coredumpActivity.this, "Please wait...", "Apply MA & Artemis traces in Progress");
+                progressDialog = ProgressDialog.show(Trace_in_coredumpActivity.this, "Please wait...", "Apply traces in Progress");
 
-                    new Thread(new Runnable() {
-                        @Override
-                        public void run() {
-                            try {
-                                if ((((CompoundButton) button_enable_ma_artemis_coredump).isChecked()) && (trace_value==0)) {
-                                    /*Enable MA & Artemis traces*/
-                                    trace_value=1;
-                                    writeSimple("/dev/gsmtty1","at+trace=1\r\n");
-                                    android.os.SystemClock.sleep(1000);
-                                    writeSimple("/dev/gsmtty1","at+xsystrace=1,\"bb_sw=1;3g_sw=1\",,\"oct=4\"\r\n");
-                                    android.os.SystemClock.sleep(2000);
-                                } else {
-                                    /*Disable traces*/
-                                    trace_value=0;
-                                    writeSimple("/dev/gsmtty1","at+trace=0\r\n");
-                                    android.os.SystemClock.sleep(1000);
-                                    writeSimple("/dev/gsmtty1","at+xsystrace=0\"\r\n");
-                                    android.os.SystemClock.sleep(2000);
-                                }
-                                progressDialog.dismiss();
-                            } catch (IOException e) {
-                                e.printStackTrace();
-                            }
-                        }
-                    }).start();
-                    Save_status_coredump();
-                }
-            }
-        });
-
-        /*Listener for button_apply_activate_coredump*/
-        button_apply_ma_artemis_digrf_coredump.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View arg0) {
-                if ((((CompoundButton) button_enable_ma_artemis_coredump).isChecked())||((((CompoundButton) button_enable_ma_artemis_digrf_coredump).isChecked()) && (trace_value!=0))) {
-                    /*Trace already running ?*/
-                    trace_unavailable();
-                } else {
-                    progressDialog = ProgressDialog.show(Trace_in_coredumpActivity.this, "Please wait...", "Apply MA & Artemis & Digrf traces in Progress");
-
-                    new Thread(new Runnable() {
-                        @Override
-                        public void run() {
-                            try {
-                                if (((CompoundButton) button_enable_ma_artemis_digrf_coredump).isChecked()) {
-                                    /*Useful for digrf traces*/
-                                    trace_value=2;
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        try {
+                            if (((CompoundButton) button_trace_ma_coredump).isChecked()) {
+                                /*Enable OCT port*/
+                                if (xsio_value == 0) {
                                     writeSimple("/dev/gsmtty1","at+xsio=2\r\n");
                                     android.os.SystemClock.sleep(1000);
-                                } else {
-                                    /*Disable traces*/
-                                    trace_value=0;
-                                    writeSimple("/dev/gsmtty1","at+xsio=0\r\n");
-                                    android.os.SystemClock.sleep(1000);
-                                    writeSimple("/dev/gsmtty1","at+trace=0\r\n");
-                                    android.os.SystemClock.sleep(1000);
-                                    writeSimple("/dev/gsmtty1","at+xsystrace=0\"\r\n");
-                                    android.os.SystemClock.sleep(2000);
+                                    xsio_value = 2;
                                 }
-                                progressDialog.dismiss();
-                            } catch (IOException e) {
-                                e.printStackTrace();
-                            }
-                        }
-                    }).start();
-                    RebootMessage();
-                    Save_status_coredump();
-                }
-            }
-        });
-
-        button_apply_activate_coredump.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View arg0) {
-                if (((CompoundButton) button_enable_ma_artemis_coredump).isChecked()) {
-                    /*Others traces enabled ?*/
-                    wrong_trace_enabled();
-                } else if (((CompoundButton) button_disable_ma_artemis_digrf_coredump).isChecked()){
-                    /*Traces not enabled ?*/
-                    trace_not_enabled();
-                } else {
-                    progressDialog = ProgressDialog.show(Trace_in_coredumpActivity.this, "Please wait...", "Apply MA & Artemis & Digrf traces in Progress");
-
-                    new Thread(new Runnable() {
-                        @Override
-                        public void run() {
-                            try {
-                                writeSimple("/dev/gsmtty1","at+trace=1\r\n");
+                                /*Enable MA traces*/
+                                writeSimple("/dev/gsmtty1","at+trace=,115200,\"st=1,pr=1,bt=1,ap=0,db=1,lt=0,li=1,ga=0,ae=0\"\r\n");
+                                android.os.SystemClock.sleep(1000);
+                                writeSimple("/dev/gsmtty1","at+xsystrace=1,\"digrf=0;bb_sw=1;3g_sw=0\",\"digrf=0x00\",\"oct=4\"\r\n");
+                                android.os.SystemClock.sleep(2000);
+                            }  else if (((CompoundButton) button_trace_ma_artemis_coredump).isChecked()) {
+                                /*Enable OCT port*/
+                                if (xsio_value == 0) {
+                                    writeSimple("/dev/gsmtty1","at+xsio=2\r\n");
+                                    android.os.SystemClock.sleep(1000);
+                                    xsio_value = 2;
+                                }
+                                /*Enable MA and Artemis traces*/
+                                writeSimple("/dev/gsmtty1","at+trace=,115200,\"st=1,pr=1,bt=1,ap=0,db=1,lt=0,li=1,ga=0,ae=1\"\r\n");
+                                android.os.SystemClock.sleep(1000);
+                                writeSimple("/dev/gsmtty1","at+xsystrace=1,\"digrf=0;bb_sw=1;3g_sw=1\",\"digrf=0x00\",\"oct=4\"\r\n");
+                                android.os.SystemClock.sleep(2000);
+                            } else if (((CompoundButton) button_trace_ma_artemis_digrf_coredump).isChecked()) {
+                                /*Enable OCT port*/
+                                if (xsio_value == 0) {
+                                    writeSimple("/dev/gsmtty1","at+xsio=2\r\n");
+                                    android.os.SystemClock.sleep(1000);
+                                    xsio_value = 2;
+                                }
+                                /*Enable MA and Artemis traces*/
+                                writeSimple("/dev/gsmtty1","at+trace=,115200,\"st=1,pr=1,bt=1,ap=0,db=1,lt=0,li=1,ga=0,ae=1\"\r\n");
                                 android.os.SystemClock.sleep(1000);
                                 writeSimple("/dev/gsmtty1","at+xsystrace=1,\"digrf=1;bb_sw=1;3g_sw=1\",\"digrf=0x84\",\"oct=4\"\r\n");
                                 android.os.SystemClock.sleep(2000);
-                                progressDialog.dismiss();
-                            } catch (IOException e) {
-                                e.printStackTrace();
+                            }  else { /*Button None checked*/
+                                /*Enable usb port*/
+                                writeSimple("/dev/gsmtty1","at+xsio=0\r\n");
+                                android.os.SystemClock.sleep(1000);
+                                xsio_value = 0;
+                                /*Disable trace*/
+                                writeSimple("/dev/gsmtty1","at+trace=0,115200,\"st=0,pr=0,bt=0,ap=0,db=0,lt=0,li=0,ga=0,ae=0\"\r\n");
+                                android.os.SystemClock.sleep(1000);
+                                /*Disable xsystrace*/
+                                writeSimple("/dev/gsmtty1","at+xsystrace=0\r\n");
+                                android.os.SystemClock.sleep(2000);
                             }
+                            progressDialog.dismiss();
+                        } catch (IOException e) {
+                            e.printStackTrace();
                         }
-                    }).start();
-                    Save_status_coredump();
-                }
+                    }
+                }).start();
+                Save_status_coredump();
+                RebootMessage();
             }
+
         });
     }
 
@@ -212,17 +150,17 @@ public class Trace_in_coredumpActivity extends Activity {
         SharedPreferences.Editor editor = preferences.edit();
 
         /*Put the values from the UI*/
-        boolean button_disable_ma_artemis_coredump_value = ((CompoundButton) button_disable_ma_artemis_coredump).isChecked();
-        boolean button_enable_ma_artemis_coredump_value = ((CompoundButton) button_enable_ma_artemis_coredump).isChecked();
-        boolean button_disable_ma_artemis_digrf_coredump_value = ((CompoundButton) button_disable_ma_artemis_digrf_coredump).isChecked();
-        boolean button_enable_ma_artemis_digrf_coredump_value = ((CompoundButton) button_enable_ma_artemis_digrf_coredump).isChecked();
+        boolean button_trace_ma_coredump_value = ((CompoundButton) button_trace_ma_coredump).isChecked();
+        boolean button_trace_ma_artemis_coredump_value = ((CompoundButton) button_trace_ma_artemis_coredump).isChecked();
+        boolean button_trace_ma_artemis_digrf_coredump_value = ((CompoundButton) button_trace_ma_artemis_digrf_coredump).isChecked();
+        boolean button_trace_none_coredump_value = ((CompoundButton) button_trace_none_coredump).isChecked();
 
         /*Value to store*/
-        editor.putBoolean("button_disable_ma_artemis_coredump_value", button_disable_ma_artemis_coredump_value);
-        editor.putBoolean("button_enable_ma_artemis_coredump_value", button_enable_ma_artemis_coredump_value);
-        editor.putBoolean("button_disable_ma_artemis_digrf_coredump_value", button_disable_ma_artemis_digrf_coredump_value);
-        editor.putBoolean("button_enable_ma_artemis_digrf_coredump_value", button_enable_ma_artemis_digrf_coredump_value);
-        editor.putInt("trace_value", trace_value);
+        editor.putBoolean("button_trace_ma_coredump_value", button_trace_ma_coredump_value);
+        editor.putBoolean("button_trace_ma_artemis_coredump_value", button_trace_ma_artemis_coredump_value);
+        editor.putBoolean("button_trace_ma_artemis_digrf_coredump_value", button_trace_ma_artemis_digrf_coredump_value);
+        editor.putBoolean("button_trace_none_coredump_value", button_trace_none_coredump_value);
+        editor.putInt("xsio_value", xsio_value);
 
         /*Commit to storage*/
         editor.commit();
