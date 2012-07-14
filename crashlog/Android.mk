1643a471ba301877446aed1ceb04559db51d6e6c
Bruce Beare <bruce.j.beare@intel.com>
2012-07-13 20:49:34 -0700

    build: make various subsystems selectable
    BZ: 46710
    
    For bringup and support of various hardware, we need to be able to disable
    intel add-on's in a convenient way via BoardConfig.mk. This commit adds
    required fixes to various Android.mk files.
    
    BoardConfig.mk is also changed disable a number of subsystems for bring-up
    of JB
    
    Change-Id: Ia00e8e8303de6bc2132cdf36dd753ab8c224255e
    Signed-off-by: Bruce Beare <bruce.j.beare@intel.com>
    Reviewed-on: http://android.intel.com:8080/56376
    Reviewed-by: Li, XiaojingX <xiaojingx.li@intel.com>
    Tested-by: Li, XiaojingX <xiaojingx.li@intel.com>

--- crashlog/Android.mk
+++ crashlog/Android.mk
@@ -16,16 +16,15 @@
 #
 
 LOCAL_PATH:= $(call my-dir)
-include $(CLEAR_VARS)
+ifneq ($(INTEL_CRASHLOGD),false)
 
+include $(CLEAR_VARS)
 LOCAL_SRC_FILES:= crashlogd.c
 
 LOCAL_MODULE_TAGS := eng debug
 LOCAL_MODULE:= crashlogd
 
 LOCAL_UNSTRIPPED_PATH := $(TARGET_ROOT_OUT_UNSTRIPPED)
-
-#LOCAL_STATIC_LIBRARIES:= libc libcutils
 
 LOCAL_SHARED_LIBRARIES:= libparse_stack libc libcutils
 include $(BUILD_EXECUTABLE)
@@ -34,3 +33,4 @@ PRODUCT_COPY_FILES += \
         $(LOCAL_PATH)/analyze_crash:system/bin/analyze_crash \
         $(LOCAL_PATH)/del_hist.sh:system/bin/del_hist.sh \
         $(LOCAL_PATH)/del_log.sh:system/bin/del_log.sh
+endif
