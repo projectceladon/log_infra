f141eda28b343ab3dd7a1776d93e6bb616ca20b9
Vidoine, Charles-EdouardX <charles.edouardx.vidoine@intel.com>
2013-06-18 17:13:53 +0200

    CLOTA: Add crash package
    BZ: 97902
    
    Create a phony package to build all crash reporting components.
    
    Change-Id: I1478ddee9de6dcb202cda5425a705ea7d55f2de5
    Signed-off-by: Vidoine, Charles-EdouardX <charles.edouardx.vidoine@intel.com>
    Reviewed-on: http://android.intel.com:8080/114138
    Reviewed-by: Imougar, JacquesX <jacquesx.imougar@intel.com>
    Reviewed-by: Benoit, NicolasX <nicolasx.benoit@intel.com>
    Reviewed-by: Seibel, Eric <eric.seibel@intel.com>
    Tested-by: Imougar, JacquesX <jacquesx.imougar@intel.com>
    Reviewed-by: buildbot <buildbot@intel.com>
    Tested-by: buildbot <buildbot@intel.com>

---
+++ Android.mk
@@ -0,0 +1,17 @@
+LOCAL_PATH:= $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := crash_package
+
+LOCAL_MODULE_TAGS := optional
+LOCAL_REQUIRED_MODULES :=  \
+    CrashReport \
+    crashinfo \
+    com.google.gson \
+    com.google.gson.xml \
+    logconfig \
+    crashparsing \
+    crashparsing.xml
+
+include $(BUILD_PHONY_PACKAGE)
+include $(call first-makefiles-under,$(LOCAL_PATH))
