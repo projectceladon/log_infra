772336e8defebbdd11f46a742792c817cbb57bce
Guilhem IMBERTON <guilhem.imberton@intel.com>
2014-02-11 11:05:25 +0100

    [SDK] Removing eng debug and user flags from components makefiles
    BZ: 143553
    
    - Kexec tools are not anymore compatible with ARM let's disable it ..
    
    Need a real fix
    
    Change-Id: I68b837bb75f13d2c05630498b7a0ab2a89bd4a2c
    Signed-off-by: Guilhem IMBERTON <guilhem.imberton@intel.com>

--- crashinfo/Android.mk
+++ crashinfo/Android.mk
@@ -4,7 +4,7 @@ LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE := crashinfo
-LOCAL_MODULE_TAGS := eng debug
+LOCAL_MODULE_TAGS := optional
 LOCAL_SRC_FILES := $(call all-subdir-java-files)
 LOCAL_JAVA_LIBRARIES := com.google.gson crashparsing
 LOCAL_CERTIFICATE := platform
