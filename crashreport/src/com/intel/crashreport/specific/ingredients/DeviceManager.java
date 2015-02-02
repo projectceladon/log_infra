b46ee94e89470235b6e9eceafefc8e31b193ec94
Nicolae Natea <nicolaex.natea@intel.com>
2015-02-02 10:59:40 +0100

    crashreport : crash when several MPANIC not ready occur
    Change-Id: Ib3603c61cab6cd769808873af88800cd06b753ab
    Tracked-On: IMINAN-19833
    Signed-off-by: Nicolae Natea <nicolaex.natea@intel.com>
    Reviewed-on: 326429

--- crashreport/src/com/intel/crashreport/specific/ingredients/DeviceManager.java
+++ crashreport/src/com/intel/crashreport/specific/ingredients/DeviceManager.java
@@ -23,6 +23,7 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
@@ -70,10 +71,6 @@ public enum DeviceManager {
 		lMpanicNotReady.add(aEventid);
 	}
 
-	public void removeEventMPanicNotReady(String aEventid) {
-		lMpanicNotReady.remove(aEventid);
-	}
-
 	public void checkMpanicNotReady(EventDB db) {
 		if (lMpanicNotReady.isEmpty()) {
 			return;
@@ -84,7 +81,9 @@ public enum DeviceManager {
 			return;
 		}
 		//we have modem events to update!
-		for (String sId : lMpanicNotReady){
+		Iterator<String> it = lMpanicNotReady.iterator();
+		while (it.hasNext()){
+			String sId = it.next();
 			Cursor cursor = db.getEventFromId(sId);
 			Event curEvent = db.fillEventFromCursor(cursor);
 			cursor.close();
@@ -111,7 +110,7 @@ public enum DeviceManager {
 			}
 			//finally, we tag the event as ready
 			db.updateEventDataReady(sId);
-			removeEventMPanicNotReady(sId);
+			it.remove();
 		}
 	}
 }
