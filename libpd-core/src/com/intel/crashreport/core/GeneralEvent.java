bbd5578f3d77c5f7279f4cf06ae7ddbcdfdab5f8
chenxia1 <xiao.x.chen@intel.com>
2019-07-25 10:51:33 +0800

    log_infra: modify license for opensource phone doctor
    Change the license to Apache License 2.0
    Clear the trademark & brands in the source files
    Remove the crashtool server and intel certificate,
    Modify readme.
    
    Change-Id: I9e39a2c57cfde13d619f07124e3d4534ab45604a
    Tracked-On: OAM-84273
    Signed-off-by: chenxia1 <xiao.x.chen@intel.com>
    Reviewed-on: 676161

--- libpd-core/src/com/intel/crashreport/core/GeneralEvent.java
+++ libpd-core/src/com/intel/crashreport/core/GeneralEvent.java
@@ -1,24 +1,17 @@
-/* INTEL CONFIDENTIAL
- * Copyright 2015 Intel Corporation
+/* Copyright (C) 2019 Intel Corporation
  *
- * The source code contained or described herein and all documents
- * related to the source code ("Material") are owned by Intel
- * Corporation or its suppliers or licensors. Title to the Material
- * remains with Intel Corporation or its suppliers and
- * licensors. The Material contains trade secrets and proprietary
- * and confidential information of Intel or its suppliers and
- * licensors. The Material is protected by worldwide copyright and
- * trade secret laws and treaty provisions. No part of the Material
- * may be used, copied, reproduced, modified, published, uploaded,
- * posted, transmitted, distributed, or disclosed in any way without
- * Intel's prior express written permission.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * No license under any patent, copyright, trade secret or other
- * intellectual property right is granted to or conferred upon you
- * by disclosure or delivery of the Materials, either expressly, by
- * implication, inducement, estoppel or otherwise. Any license under
- * such intellectual property rights must be express and approved by
- * Intel in writing.
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package com.intel.crashreport.core;
@@ -491,7 +484,7 @@ public class GeneralEvent {
 		// do not use "uniquekey" of crashtool object, it is for internal use only
 		//uniqueKeyComponents should be used
 		sBuild.setUniqueKeyComponents(Utils.parseUniqueKey(this.uniqueKeyComponent));
-		sBuild.setOrganization(com.intel.crashtoolserver.bean.Build.ORGANIZATION_MCG);
+		sBuild.setOrganization(com.intel.crashtoolserver.bean.Build.DEFAULT_ORGANIZATION);
 
                 com.intel.crashtoolserver.bean.TestCase tc = null;
 
@@ -504,7 +497,7 @@ public class GeneralEvent {
 				getType(), getData0(), getData1(), getData2(), getData3(),
 				getData4(), getData5(), date, convertUptime(getUptime()),
 				null /*logfile*/, sBuild,
-				com.intel.crashtoolserver.bean.Event.Origin.CLOTA, device,
+				com.intel.crashtoolserver.bean.Event.Origin.NULL, device,
 				getiRowID(), pdStatus, this.osBootMode,
 				new com.intel.crashtoolserver.bean.Modem(getModemVersionUsed()),
 				getCrashDir(), uploaded, logUploaded, tc);
--- libpd-crashtool/crashtoolserver/bean/Event.java
+++ libpd-crashtool/crashtoolserver/bean/Event.java
@@ -1,24 +1,17 @@
-/* INTEL CONFIDENTIAL
- * Copyright 2015 Intel Corporation
+/* Copyright (C) 2019 Intel Corporation
  *
- * The source code contained or described herein and all documents
- * related to the source code ("Material") are owned by Intel
- * Corporation or its suppliers or licensors. Title to the Material
- * remains with Intel Corporation or its suppliers and
- * licensors. The Material contains trade secrets and proprietary
- * and confidential information of Intel or its suppliers and
- * licensors. The Material is protected by worldwide copyright and
- * trade secret laws and treaty provisions. No part of the Material
- * may be used, copied, reproduced, modified, published, uploaded,
- * posted, transmitted, distributed, or disclosed in any way without
- * Intel's prior express written permission.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * No license under any patent, copyright, trade secret or other
- * intellectual property right is granted to or conferred upon you
- * by disclosure or delivery of the Materials, either expressly, by
- * implication, inducement, estoppel or otherwise. Any license under
- * such intellectual property rights must be express and approved by
- * Intel in writing.
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package com.intel.crashtoolserver.bean;
@@ -36,9 +29,6 @@ import com.intel.crashtool.util.HashGenerator;
 /**
  * Bean Event, used to transmit event data between a client and a server
  *
- * Be aware that bean is used by CLOTA and MPTA, do not rename package and class
- * name for down ward.
- *
  * @author mauretx
  */
 public class Event implements Serializable {
@@ -50,16 +40,16 @@ public class Event implements Serializable {
 	 * Origin exhaustive list
 	 * default value : GENE
 	 */
-	public enum Origin {ACS, CLA, CLOTA, EGG, GENE, MPM, MPTA, PDN, PFT, SRC, STUB}
-	
+	public enum Origin {NULL}
+
 	public static final int UPLOADED_STATUS_INVALID = -1;
 	public static final int UPLOADED_STATUS_NOT_UPLOADED = 0;
 	public static final int UPLOADED_STATUS_UPLOADED = 1;
-	
+
 	public static final int LOGFILE_UPLOADED_STATUS_INVALID = -1;
 	public static final int LOGFILE_UPLOADED_STATUS_NOT_UPLOADED = 0;
-	public static final int LOGFILE_UPLOADED_STATUS_UPLOADED = 1;	
-	
+	public static final int LOGFILE_UPLOADED_STATUS_UPLOADED = 1;
+
 	/**
 	 * rowid default value : GENE
 	 */
